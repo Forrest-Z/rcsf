9825590a4fcbf6229cde5bc8d55a1b732018e9ba
yurui <18507132980@163.com>
2021-06-08 16:51:29 +0800

    add vehicle list panel

--- src/views/dashboard/panels/VehicleList.js
+++ src/views/dashboard/panels/VehicleList.js
@@ -1,7 +1,13 @@
+// ** React Imports
 import React, { Component } from 'react'
-import { Card, CardHeader, CardBody, CardTitle, Button } from 'reactstrap'
-import { List, Maximize, Minimize, Move } from 'react-feather'
 
+// ** Third Party Components
+import { Card, CardHeader, CardBody, CardTitle, Button, ListGroup, ListGroupItem, Badge, Row, Col } from 'reactstrap'
+import { List, Maximize, Minimize, Move } from 'react-feather'
+import { FcHighBattery, FcChargeBattery } from 'react-icons/fc'
+
+// ** Custom Components
+import Avatar from '@components/avatar'
 export class VehicleList extends Component {
   constructor() {
     super()
@@ -43,8 +49,101 @@ export class VehicleList extends Component {
             </Button.Ripple>
           </div>
         </CardHeader>
-        <CardBody>
-
+        <CardBody className='p-0'>
+          <ListGroup>
+            <ListGroupItem className='border-0'>
+              <Row>
+                <Col className='d-flex justify-content-left align-items-center' xl='7'>
+                  <Avatar className='mr-1' content='R' size='lg' status='online' color='light-primary' />
+                  <div className='d-flex flex-column mr-2'>
+                    <h4 className='text-truncate mb-0'>Robot-01</h4>
+                    <small className='text-truncate text-muted mb-0'>Sweep Robot</small>
+                  </div>
+                </Col>
+                <Col xl='5'>
+                  <div className='d-flex flex-column'>
+                    <div className='mb-50'>
+                      <FcChargeBattery size={20} />
+                      <small className='text-truncate mb-0'>30%</small>
+                    </div>
+                    <div>
+                      <Badge color='light-info'>CHARGING</Badge>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </ListGroupItem>
+            <hr className='w-100 p-0 mb-1' />
+            <ListGroupItem className='border-0'>
+              <Row>
+                <Col className='d-flex justify-content-left align-items-center' xl='7'>
+                  <Avatar className='mr-1' content='R' size='lg' status='online' color='light-primary' />
+                  <div className='d-flex flex-column mr-2'>
+                    <h4 className='text-truncate mb-0'>Robot-02</h4>
+                    <small className='text-truncate text-muted mb-0'>Sweep Robot</small>
+                  </div>
+                </Col>
+                <Col xl='5'>
+                  <div className='d-flex flex-column'>
+                    <div className='mb-50'>
+                      <FcHighBattery size={20} />
+                      <small className='text-truncate mb-0'>80%</small>
+                    </div>
+                    <div>
+                      <Badge color='light-primary'>IDLE</Badge>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </ListGroupItem>
+            <hr className='w-100 p-0 mb-1' />
+            <ListGroupItem className='border-0'>
+              <Row>
+                <Col className='d-flex justify-content-left align-items-center' xl='7'>
+                  <Avatar className='mr-1' content='R' size='lg' status='online' color='light-primary' />
+                  <div className='d-flex flex-column mr-2'>
+                    <h4 className='text-truncate mb-0'>Robot-03</h4>
+                    <small className='text-truncate text-muted mb-0'>Sweep Robot</small>
+                  </div>
+                </Col>
+                <Col xl='5'>
+                  <div className='d-flex flex-column'>
+                    <div className='mb-50'>
+                      <FcHighBattery size={20} />
+                      <small className='text-truncate mb-0'>80%</small>
+                    </div>
+                    <div>
+                      <Badge color='light-success'>PROCESS</Badge>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </ListGroupItem>
+            <hr className='w-100 p-0 mb-1' />
+            <ListGroupItem className='border-0'>
+              <Row>
+                <Col className='d-flex justify-content-left align-items-center' xl='7'>
+                  <Avatar className='mr-1' content='R' size='lg' status='online' color='light-primary' />
+                  <div className='d-flex flex-column mr-2'>
+                    <h4 className='text-truncate mb-0 text-black'>Robot-04</h4>
+                    <small className='text-truncate text-muted mb-0'>Sweep Robot</small>
+                  </div>
+                </Col>
+                <Col xl='5'>
+                  <div className='d-flex flex-column'>
+                    <div className='mb-50'>
+                      <FcHighBattery size={20} />
+                      <small className='text-truncate mb-0'>80%</small>
+                    </div>
+                    <div>
+                      <Badge color='light-danger'>ERROR</Badge>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </ListGroupItem>
+            <hr className='w-100 p-0 mb-1' />
+          </ListGroup>
         </CardBody>
       </Card>
     )
