d26549b3a4cb3d1a454e508b471bb20191400caf
ethanruiyu <pazzitul@163.com>
2021-08-02 16:48:03 +0800

    add use map action

--- src/views/map/colums.js
+++ src/views/map/colums.js
@@ -3,11 +3,20 @@ import { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
 // ** Store & Actions
-import { deleteMap } from './store/actions'
+import { deleteMap, updateMap } from './store/actions'
 import { store } from '@store/storeConfig/store'
 
 // ** Third Party Components
-import { Edit2, Eye, MoreVertical, Download, Trash, CheckCircle, XCircle, Check } from 'react-feather'
+import {
+  Edit2,
+  Eye,
+  MoreVertical,
+  Download,
+  Trash,
+  CheckCircle,
+  XCircle,
+  Check
+} from 'react-feather'
 import {
   Badge,
   UncontrolledDropdown,
@@ -23,18 +32,22 @@ export const columns = [
     minWidth: '197',
     selector: 'id',
     sortable: true,
-    cell: row => <Link to={`/apps/invoice/preview/${row.id}`}>{`# ${row.id}`}</Link>
+    cell: (row) => (
+      <Link to={`/apps/invoice/preview/${row.id}`}>{`# ${row.id}`}</Link>
+    )
   },
   {
     name: 'Name',
     minWidth: '197',
     selector: 'name',
-    cell: row => {
+    cell: (row) => {
       return (
-        <div className='d-flex justify-content-left align-items-center'>
-          <div className='d-flex flex-column'>
-            <h5 className='user-name text-truncate mb-0'>{row.name}</h5>
-            <small className='text-truncate text-muted mb-0'>{row.description}</small>
+        <div className="d-flex justify-content-left align-items-center">
+          <div className="d-flex flex-column">
+            <h5 className="user-name text-truncate mb-0">{row.name}</h5>
+            <small className="text-truncate text-muted mb-0">
+              {row.description}
+            </small>
           </div>
         </div>
       )
@@ -45,9 +58,9 @@ export const columns = [
     minWidth: '297',
     selector: 'createTime',
     sortable: true,
-    cell: row => (
+    cell: (row) => (
       <div>
-        <span className='mb-0'>{row.createTime}</span>
+        <span className="mb-0">{row.createTime}</span>
       </div>
     )
   },
@@ -56,9 +69,9 @@ export const columns = [
     minWidth: '297',
     selector: 'active',
     sortable: true,
-    cell: row => (
+    cell: (row) => (
       <div>
-        <span className='mb-0'>{row.updateTime}</span>
+        <span className="mb-0">{row.updateTime}</span>
       </div>
     )
   },
@@ -67,71 +80,89 @@ export const columns = [
     minWidth: '47',
     selector: 'using',
     sortable: true,
-    cell: row => (
+    cell: (row) => (
       <div>
-        {
-          row.active ? <Check className='text-success' size={20} /> : null
-        }
+        {row.active ? <Check className="text-success" size={20} /> : null}
       </div>
     )
   },
   {
     name: 'Action',
     selector: '',
-    cell: row => (
-      <div className='column-action d-flex align-items-center'>
-        <div className='mr-1'>
-          <Link id={`edit-tooltip-${row.id}`} to={{ pathname: 'map/edit', map: row }}>
+    cell: (row) => (
+      <div className="column-action d-flex align-items-center">
+        <div className="mr-1">
+          <Link
+            id={`edit-tooltip-${row.id}`}
+            to={{ pathname: 'map/edit', map: row }}
+          >
             <Edit2 size={17} />
           </Link>
-          <UncontrolledTooltip placement='top' target={`edit-tooltip-${row.id}`}>
+          <UncontrolledTooltip
+            placement="top"
+            target={`edit-tooltip-${row.id}`}
+          >
             Edit Map
-        </UncontrolledTooltip>
+          </UncontrolledTooltip>
         </div>
         <div>
-          <Link id={`preview-tooltip-${row.id}`} to='' >
+          <Link id={`preview-tooltip-${row.id}`} to="">
             <Eye size={17} />
           </Link>
-          <UncontrolledTooltip placement='top' target={`preview-tooltip-${row.id}`}>
+          <UncontrolledTooltip
+            placement="top"
+            target={`preview-tooltip-${row.id}`}
+          >
             Preview Map
           </UncontrolledTooltip>
         </div>
-        <div className='mx-1'>
+        <div className="mx-1">
           <UncontrolledDropdown>
-            <DropdownToggle tag='span'>
-              <MoreVertical size={17} className='cursor-pointer' />
+            <DropdownToggle tag="span">
+              <MoreVertical size={17} className="cursor-pointer" />
             </DropdownToggle>
             <DropdownMenu right>
-              <DropdownItem tag='a' href='/' className='w-100' onClick={e => e.preventDefault()}>
-                <Download size={14} className='mr-50' />
-                <span className='align-middle'>Download</span>
-              </DropdownItem>
-              <DropdownItem tag='a' href='/' className='w-100' onClick={e => e.preventDefault()}>
-                {
-                  row.active ? (
-                    <div>
-                      <XCircle size={14} className='mr-50' />
-                      <span className='align-middle'>Don't Use</span>
-                    </div>
-                  ) : (
-                      <div>
-                        <CheckCircle size={14} className='mr-50' />
-                        <span className='align-middle'>Use</span>
-                      </div>
-                    )
-                }
+              <DropdownItem
+                tag="a"
+                href="/"
+                className="w-100"
+                onClick={(e) => e.preventDefault()}
+              >
+                <Download size={14} className="mr-50" />
+                <span className="align-middle">Download</span>
               </DropdownItem>
               <DropdownItem
-                tag='a'
-                href='/'
-                className='w-100'
-                onClick={e => {
+                tag="a"
+                href="/"
+                className="w-100"
+                onClick={(e) => {
+                  e.preventDefault()
+                  store.dispatch(updateMap(row.id, {active: !row.active}))
+                }}
+              >
+                {row.active ? (
+                  <div>
+                    <XCircle size={14} className="mr-50" />
+                    <span className="align-middle">Don't Use</span>
+                  </div>
+                ) : (
+                  <div>
+                    <CheckCircle size={14} className="mr-50" />
+                    <span className="align-middle">Use</span>
+                  </div>
+                )}
+              </DropdownItem>
+              <DropdownItem
+                tag="a"
+                href="/"
+                className="w-100"
+                onClick={(e) => {
                   e.preventDefault()
                   store.dispatch(deleteMap(row.id))
                 }}
               >
-                <Trash size={14} className='mr-50' />
-                <span className='align-middle'>Delete</span>
+                <Trash size={14} className="mr-50" />
+                <span className="align-middle">Delete</span>
               </DropdownItem>
             </DropdownMenu>
           </UncontrolledDropdown>
@@ -139,5 +170,4 @@ export const columns = [
       </div>
     )
   }
-
 ]
